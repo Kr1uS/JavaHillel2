52545734ac5fca0ea9f2bdb77d174632b4375a24
Kr1uS <72558733+Kr1uS@users.noreply.github.com>
2021-10-30 13:21:53 +0300

    cw4

---
+++ src/com/company/cw4/Auto.java
@@ -0,0 +1,16 @@
+package com.company.cw4;
+
+public class Auto {
+
+    static class BMW {
+        public void printText() {
+            System.out.println("BMW");
+        }
+    }
+
+    static class Audi {
+        public void printText() {
+            System.out.println("Audi");
+        }
+    }
+}
---
+++ src/com/company/cw4/Main.java
@@ -0,0 +1,9 @@
+package com.company.cw4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Auto.Audi audi = new Auto.Audi();
+        audi.printText();
+    }
+}
