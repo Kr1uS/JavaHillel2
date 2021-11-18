8c82f0b9ff11b7cec875bdc9daae3fb46046fe24
Kirill <72558733+Kr1uS@users.noreply.github.com>
2021-11-18 21:53:14 +0200

    Delete src/com/company/cw5/chess directory

--- src/com/company/cw5/chess/items/HasValue.java
+++
@@ -1,7 +0,0 @@
-package com.company.cw5.chess.items;
-
-public interface HasValue {
-
-    int getValue();
-
-}
--- src/com/company/cw5/chess/items/Pawn.java
+++
@@ -1,8 +0,0 @@
-package com.company.cw5.chess.items;
-
-public class Pawn extends ChessItem {
-    @Override
-    public void draw() {
-        System.out.println("Pawn");
-    }
-}
